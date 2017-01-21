808ae7de8bf2e0045f1104e72e108cdf75789e81
Kevin <kmather73@users.noreply.github.com>
2017-01-21 10:35:33 -0600

    Create 250.cpp

---
+++ SRM-706-DIV2/250.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class ThreeIncreasing {
+public:
+	int minEaten(int a, int b, int c){
+		int count = 0;
+		if( c <= b){
+			count += b-c+1;
+			b = c-1;
+		}
+		
+		if(b <= a){
+			count += a-b+1;
+			a = b-1;
+		}
+		
+		if( a <=  0) return -1;
+		return count;
+	}
+};
