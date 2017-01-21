4c358675de5b7a6e96b373de26b901aac427c755
Kevin <kmather73@users.noreply.github.com>
2017-01-21 10:35:56 -0600

    Create 500.cpp

---
+++ SRM-706-DIV2/500.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class SellingFruits {
+public:	
+	int maxDays(int x, int f, int d, int p){
+		int days = min(f, d/x);
+		f -= days;
+		d -= x*days;
+		days += d / (x+p);
+		
+		return days;
+	}	
+};
